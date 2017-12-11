af562dbb02210bc848393a8cf876a6cbe34dfca7
Andrew <heldiggris7@gmail.com>
2017-12-11 10:46:54 -0600

    update Friends List

--- Chapter8/Chapter8/Menu.Designer.vb
+++ Chapter8/Chapter8/Menu.Designer.vb
@@ -23,6 +23,9 @@ Partial Class Menu
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnLottery = New System.Windows.Forms.Button()
+        Me.btnFriends = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
@@ -34,11 +37,41 @@ Partial Class Menu
         Me.Button1.Text = "Seating Chart"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'btnLottery
+        '
+        Me.btnLottery.Location = New System.Drawing.Point(12, 59)
+        Me.btnLottery.Name = "btnLottery"
+        Me.btnLottery.Size = New System.Drawing.Size(74, 41)
+        Me.btnLottery.TabIndex = 1
+        Me.btnLottery.Text = "Lottery"
+        Me.btnLottery.UseVisualStyleBackColor = True
+        '
+        'btnFriends
+        '
+        Me.btnFriends.Location = New System.Drawing.Point(92, 12)
+        Me.btnFriends.Name = "btnFriends"
+        Me.btnFriends.Size = New System.Drawing.Size(74, 41)
+        Me.btnFriends.TabIndex = 2
+        Me.btnFriends.Text = "Friends"
+        Me.btnFriends.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(223, 225)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(49, 25)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
         'Menu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnFriends)
+        Me.Controls.Add(Me.btnLottery)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Menu"
         Me.Text = "Menu"
@@ -46,4 +79,7 @@ Partial Class Menu
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents btnLottery As System.Windows.Forms.Button
+    Friend WithEvents btnFriends As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
 End Class
--- Chapter8/Chapter8/Menu.vb
+++ Chapter8/Chapter8/Menu.vb
@@ -1,7 +1,22 @@
 ﻿Public Class Menu
 
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim Box = New SeatingChart
         Box.Show()
     End Sub
+
+    Private Sub btnLottery_Click(sender As Object, e As EventArgs) Handles btnLottery.Click
+        Dim Box = New Lottery
+        Box.Show()
+    End Sub
+
+    Private Sub btnFriends_Click(sender As Object, e As EventArgs) Handles btnFriends.Click
+        Dim Box = New FriendsList
+        Box.Show()
+    End Sub
+
 End Class
